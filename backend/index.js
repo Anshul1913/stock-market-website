074fa0b4f8f27bcdfe1a3f7104305ea78ac7f9db
anshul <94953989+Anshul1913@users.noreply.github.com>
2025-09-28 00:13:40 +0530

    update frontend url link

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import bodyParser from "body-parser";
 const app = express();
 
 app.use(cors({
-  origin: "https://stock-market-website-nine.vercel.app",
+  origin: "https://traders-profit-club.vercel.app/",
   methods: ["GET", "POST"],
   credentials: true
 }));
