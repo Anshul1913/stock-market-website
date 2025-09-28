4627888b5e0edd59cb1bc04e6e31ffb235614961
anshul <94953989+Anshul1913@users.noreply.github.com>
2025-09-28 12:34:38 +0530

    update port

--- backend/index.js
+++ backend/index.js
@@ -45,5 +45,7 @@ app.post("/contact", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 10000;
+const PORT =  10000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
